1ba14a9c6f555d3caae119dbbfd2b7f56156a4bc
Denis Sh <dshebut@rambler.ru>
2025-10-02 01:52:44 +0300

    fast test

--- front/screens/new/AuthScreen.js
+++ front/screens/new/AuthScreen.js
@@ -1,9 +1,12 @@
-import { Button, ButtonText, Card, Center, Heading, Input, InputField, Text, VStack } from "@gluestack-ui/themed"
+import { Button, ButtonText, Card, Center, Heading, Input, InputField, ScrollView, Text, VStack } from "@gluestack-ui/themed"
 import { useEffect, useState } from "react"
 import { SESSION_TOKEN, loginRequest } from "../../api/new/login"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { EMPLOYEE_NUMBER_ROUTE, QA_EMPLOYEE_ROLE_ROUTE, REGISTRATION_SCREEN_ROUTE, TOOLS_SCANNER_ROUTE, WAREHOUSE_EMPLOYEE_ROUTE, WORKERS_PIPELINE_ROLE_ROUTE } from "./Screens"
 import { ADMIN_ROLE, QA_EMPLOYEE_ROLE, WAREHOUSE_EMPLOYEE_ROLE, WORKER_ROLE, WORKERS_PIPELINE_ROLE } from "../../api/new/register"
+import { getDocumentAsync } from "expo-document-picker"
+import { sendImageToPredictRequest } from "../../api/new/send_image/send_image_to_predict"
+import { sendZipToPredictRequest } from "../../api/new/send_image/send_zip_to_predict"
 
 const AuthScreen = ({ navigation }) => {
     useEffect(() => {
@@ -35,31 +38,71 @@ const AuthScreen = ({ navigation }) => {
     }
     const [login, setLogin] = useState("")
     const [password, setPassword] = useState("")
+
+
+    const onUploadPhotoClick = async () => {
+        var result = await getDocumentAsync({ type: ["image/jpeg", "image/png"] })
+        if (result.assets.length > 0) {
+            sendImageToPredictRequest({
+                file: result.assets[0],
+                onSuccess: (data) => { }
+            })
+        }
+    }
+
+    const onUploadZipClick = async () => {
+        var result = await getDocumentAsync({ type: 'application/zip' })
+        if (result.assets.length > 0) {
+            sendZipToPredictRequest({
+                file: result.assets[0],
+                onSuccess: (data) => { }
+            })
+        }
+    }
+
     return (
-        <Center p="$10">
-            <Card>
+        <ScrollView>
+            <Center p="$10">
                 <VStack space="md">
-                    <Heading>Авторизация</Heading>
-                    <Text>Логин</Text>
-                    <Input>
-                        <InputField
-                            inputMode="decimal"
-                            value={login}
-                            placeholder="Табельный номер" onChangeText={setLogin} />
-                    </Input>
-                    <Text>Пароль</Text>
-                    <Input>
-                        <InputField value={password} type="password" placeholder="Пароль" onChangeText={setPassword} />
-                    </Input>
-                    <Button onPress={onLogin}>
-                        <ButtonText>Войти</ButtonText>
-                    </Button>
-                    <Button variant="outline" onPress={onRegistration}>
-                        <ButtonText>Регистрация</ButtonText>
-                    </Button>
+                    <Card>
+                        <VStack space="md">
+                            <Heading>Авторизация</Heading>
+                            <Text>Логин</Text>
+                            <Input>
+                                <InputField
+                                    inputMode="decimal"
+                                    value={login}
+                                    placeholder="Табельный номер" onChangeText={setLogin} />
+                            </Input>
+                            <Text>Пароль</Text>
+                            <Input>
+                                <InputField value={password} type="password" placeholder="Пароль" onChangeText={setPassword} />
+                            </Input>
+                            <Button onPress={onLogin}>
+                                <ButtonText>Войти</ButtonText>
+                            </Button>
+                            <Button variant="outline" onPress={onRegistration}>
+                                <ButtonText>Регистрация</ButtonText>
+                            </Button>
+                        </VStack>
+                    </Card>
+                    <Card>
+                        <VStack space='sm'>
+                            <Heading>Быстрый тест без логина</Heading>
+                            <Text>Загрузите фото или архив с несколькими фото.</Text>
+                            <Text>{`Архив с результатами распознавания будет сохранен\nв Загрузки через пару секунд.`}</Text>
+                            <Button variant='outline' onPress={onUploadPhotoClick}>
+                                <ButtonText>Загрузить фото</ButtonText>
+                            </Button>
+                            <Button variant='outline' onPress={onUploadZipClick}>
+                                <ButtonText>Загрузить архив</ButtonText>
+                            </Button>
+                        </VStack>
+                    </Card>
                 </VStack>
-            </Card>
-        </Center>
+            </Center>
+        </ScrollView>
+
     )
 }
 export default AuthScreen;
