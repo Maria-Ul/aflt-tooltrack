6cb70578dcd331b1f74f2a02e07ea32444646fea
Danil Bolotov <davbolotov@prm.beeline.ru>
2025-10-16 23:53:06 +0500

    Доработки

--- front/api/baseApi.js
+++ front/api/baseApi.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-//185.44.167.225
-export const BACKEND_URL = "http://185.44.167.225:8000"
-export const WEB_SOCKET_URL = "ws://185.44.167.225:8000"
+
+export const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_REST || "http://localhost:8000";
+export const WEB_SOCKET_URL = process.env.EXPO_PUBLIC_BACKEND_WS || "ws://localhost:8000";
+
 export const afltToolscanApi = axios.create(
     {
         baseURL: BACKEND_URL,
